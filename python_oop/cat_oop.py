c89b878df311543f99f7c486e6d8ca825d873c4e
Aaron Smith <aaroncosmith@gmail.com>
2020-01-21 10:19:24 -0500

    missing a :, now it takes the arguement self

--- python_oop/cat_oop.py
+++ python_oop/cat_oop.py
@@ -1,10 +1,10 @@
 class Cat:
-    species = "mammal"
+    species = 'mammal'
     legs = 4
     eyes = 2
     tail = 1
 
-    def __init__(self, name, age)
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
@@ -12,3 +12,7 @@ class Cat:
         return "{} is {} years old".format(self.name, self.age)
 
 gus = Cat("Gus", 9)
+
+print(gus.description())
+# print(Cat.description) will not work.
+
